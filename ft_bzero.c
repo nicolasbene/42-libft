4eb85c7b461282828e92c8b9bfcc9146286b004e
Nicolas BENOIT <nibenoit@e1r12p19.clusters.42paris.fr>
2022-11-13 15:29:26 +0100

    modif_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,25 +6,12 @@
 /*   By: nibenoit <nibenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 15:45:53 by nibenoit          #+#    #+#             */
-/*   Updated: 2022/11/12 18:20:29 by nibenoit         ###   ########.fr       */
+/*   Updated: 2022/11/13 15:26:49 by nibenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// void	ft_bzero(void *pointer, size_t count)
-// {
-// 	size_t	i;
-// 	char	*ptr;
-
-// 	i = 0;
-// 	ptr = (char *)pointer;
-// 	while (i < count)
-// 	{
-// 		ptr[i] = 0;
-// 		i++;
-// 	}
-// }
 void	ft_bzero(void *ptr, size_t size)
 {
 	char	*temp;
